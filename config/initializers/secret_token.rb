bd8f533bb3c5d5284b6758bda6f44a214c4ccb67
Eric Matzner <eric@natmobi.com>
2012-09-14 17:26:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a2bb6431f9e5c6b0a39c7fa4ef80cd43c05239b83db0d7739a3175c352205322d6ed91fe4ff4dfaf0a27b91a0e4aa8e9b0828b51202f151e09f82c0286682efa'
